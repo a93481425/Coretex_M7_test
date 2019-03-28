e2cc7bafe6cec316574f39f1df7ec1ab1667034a
a93481425 <a93481425@gmail.com>
2019-03-29 01:23:35 +0800

    20190329 work it

--- Cortex_M7_test/main.c
+++ Cortex_M7_test/main.c
@@ -3,7 +3,21 @@
 
 int main(){
     pinMode(GPIO_PORTI,1,OUTPUT);
+    unsigned int i;
+  
     while(1){
-     digitalWrite(GPIO_PORTI,1,HIGH);
+                digitalWrite(GPIO_PORTI,1,HIGH);
+
+		for (i = 0; i < 100000; i++)
+			;
+
+		//reset GPIOD led pin
+		digitalWrite(GPIO_PORTI,1,LOW);
+
+		for (i = 0; i < 100000; i++)
+			;
+
+
+     
     }
 }
